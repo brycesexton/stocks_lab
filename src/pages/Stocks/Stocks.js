1fcfdb8a5c56c7ca44c9e8d4ed48ea2196a95ded
Bryce Sexton <brycesexton15@gmail.com>
2024-02-13 19:55:10 -0800

    changed stocks.js

--- src/pages/Stocks/Stocks.js
+++ src/pages/Stocks/Stocks.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
-export default function Stocks (props) {
-  const Stocks = [
+export default function Stocks(props) {
+  const stocks = [
     {name: "Apple Inc.", symbol: "AAPL", lastPrice: 140.64, change: -0.280000000000001, high: 141.74, low: 140.35, open: 141.5},
     {name: "Microsoft Corporation", symbol: "MSFT", lastPrice: 64.98, change: 0.109999999999999, high: 65.45, low: 64.76, open: 65.12},
     {name: "Alphabet Inc.", symbol: "GOOGL", lastPrice: 835.14, change: -4.50999999999999, high: 844, low: 829.1, open: 842},
@@ -12,14 +12,15 @@ export default function Stocks (props) {
 
   return (
     <div className="stocks">
-      {stocks.map((coin) => {
-        const { name, symbol } = coin;
+      {stocks.map((stock) => {
+        const { name, symbol } = stock;
 
         return (
-          <Link to={`/about/${symbol}`}>
+          <Link to={`/about/${symbol}`} key={symbol}>
             <h2>{name}</h2>
           </Link>
         );
       })}
     </div>
   );
+}
